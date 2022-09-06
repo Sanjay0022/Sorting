f86344747326c16099fc9bea8f501d4303a1c0b5
Dell <Dell@10.12.37.46>
2022-09-06 14:51:04 +0530

    Bubble Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,6 +1,6 @@
-package sorting1
+package bubblesort;
 
-public class BubbleSort {  
+public class bubblesort {  
     static void bubbleSort(int[] arr) {  
         int n = arr.length;  
         int temp = 0;  
@@ -18,11 +18,12 @@ public class BubbleSort {
   
     }  
     public static void main(String[] args) {  
-                int arr[] ={3,60,35,2,45,320,5};  
+                int arr[] ={3,80,95,2,45,30,9};  
                  
                 System.out.println("Array Before Bubble Sort");  
                 for(int i=0; i < arr.length; i++){  
-                        System.out.print(arr[i] + " ");  
+                        System.out.print(arr[i] 
+                        		++ " ");  
                 }  
                 System.out.println();  
                   
